066e64d9367901827bd63e622da2588299646ebf
wenqzhen <wenqizhengcn@gmail.com>
2015-01-09 13:57:48 -0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -10,9 +10,11 @@ power <- read.table('household_power_consumption.txt', header=TRUE, sep=';',
 #--Subset data for 2/1/2007 and 2/2/2007 two days only
 powersub <- subset(power, power$Date %in% c('1/2/2007','2/2/2007'))
 
-#--plot the histogram
-plot1 <- hist(powersub$Global_active_power, col='red', main='Global Active Power',
-              xlab='Global Active Power (kilowatts)')
+#--set working directory to the coloned github repo
+setwd("~/Learning Info/Data Scientist/EDA/ExData_Plotting1")
 
-#--Export to PNG
-png(filename='plot1.png', width=480, height=480, units = 'px')
+#--Plot the histogram and export to PNG
+png(filename='plot2.png', width=480, height=480, units = 'px', bg='transparent')
+hist(powersub$Global_active_power, col='red', main='Global Active Power',
+     xlab='Global Active Power (kilowatts)')
+dev.off()
